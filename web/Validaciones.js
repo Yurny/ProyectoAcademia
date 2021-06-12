f93a938f47e38930f91a15c3b9233190a0be9ff6
Yurny <yyladino6@misena.edu.co>
2021-06-12 16:06:22 -0600

    hice la sesion

---
+++ web/Validaciones.js
@@ -0,0 +1,15 @@
+function redireccion (value){
+    
+    if (value=="Vendedor") {
+        
+        location.href="menu_Vendedor.jsp";
+        
+        
+    }else if(value=="Comprador"){
+       location.href="menu_Comprador.jsp"; 
+    }
+}
+    
+
+
+
